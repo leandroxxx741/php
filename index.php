c20c86c5c89677f6c8b184273c92bf1d7d060195
administrator@DFONTE.INTRA <administrator@DFONTE.INTRA>
2019-04-23 15:32:37 -0300

    mudei para mudanca codigo45678

--- index.php
+++ index.php
@@ -1,13 +1,8 @@
 <?php
 
-<<<<<<< HEAD
-echo "Meu primeiro código PHP123!";
-echo "mudanca codigo4567"
-=======
-echo "Meu primeiro código PHP!";
->>>>>>> cfc84983d4ba11a48b138e0a1115e4507d780470
+echo "Meu primeiro código PHP1234!";
+echo "mudanca codigo45678"
 
-echo "Mudanca codigo!";
 ?>
 
 php7.
